1e22d6031a75d12b21ef14f96078b4a3d6b3bf7b
Felipe Andrada <felipe.andrada@gmail.com>
2020-04-13 11:15:22 -0300

    create table to connect genres to events

---
+++ db/migrate/20200413141229_create_event_genres.rb
@@ -0,0 +1,10 @@
+class CreateEventGenres < ActiveRecord::Migration[5.2]
+  def change
+    create_table :event_genres do |t|
+      t.references :event, foreign_key: true
+      t.references :genre, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
